d74536bb833ea40a204914a02551341486e10bc3
maneptha <maneptha@gmail.com>
2017-10-31 03:03:33 +0100

    Got rid of browser pipeline in phoenix router.
    We will serve browser client as single page application and as static file.

--- lib/iclog_web/router.ex
+++ lib/iclog_web/router.ex
@@ -1,22 +1,12 @@
 defmodule IclogWeb.Router do
   use IclogWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", IclogWeb do
-    pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
