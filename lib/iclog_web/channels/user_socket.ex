b677d65ca20825516d5a0531918df90c5c3e2067
maneptha <maneptha@gmail.com>
2017-11-09 15:08:43 +0100

    Updates for heroku deployment.

--- lib/iclog_web/channels/user_socket.ex
+++ lib/iclog_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule IclogWeb.UserSocket do
   channel "observation:*", IclogWeb.ObservationChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
